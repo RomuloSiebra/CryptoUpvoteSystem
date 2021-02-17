72d57460548a827dfe6b03aa7db4308284d2a621
RomuloSiebra <romulosiebra147@gmail.com>
2021-02-17 01:49:09 -0300

    Generated proto files. Added Server side service with gRPC

--- server/main.go
+++ server/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+
+	upvoteSystem "github.com/RomuloSiebra/CryptoUpvoteSystem/proto/UpvoteSystem"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	upvoteSystem.UnimplementedUpvoteSystemServer
+}
+
 func main() {
+	port := 3333
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	fmt.Printf("Server listening at port: %d \n", port)
+	s := grpc.NewServer()
+	upvoteSystem.RegisterUpvoteSystemServer(s, &server{})
+
+	s.Serve(lis)
 
 }
