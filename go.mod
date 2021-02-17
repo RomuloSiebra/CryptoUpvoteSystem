86b92e18d55e67da11bb29bc77f69c33a9bf0439
RomuloSiebra <romulosiebra147@gmail.com>
2021-02-17 01:57:34 -0300

    Added connection with MongoDB

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/RomuloSiebra/CryptoUpvoteSystem
 go 1.13
 
 require (
+	go.mongodb.org/mongo-driver v1.4.6
 	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.25.0
 )
--- server/main.go
+++ server/main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
+	"go.mongodb.org/mongo-driver/mongo/options"
+
 	upvoteSystem "github.com/RomuloSiebra/CryptoUpvoteSystem/proto/UpvoteSystem"
+	"go.mongodb.org/mongo-driver/mongo"
 	"google.golang.org/grpc"
 )
+
+var dbClient *mongo.Client
+var mongoCtx context.Context
 
 type server struct {
 	upvoteSystem.UnimplementedUpvoteSystemServer
 }
 
 func main() {
-	port := 3333
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	dbClient, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mongoCtx = context.Background()
+	err = dbClient.Connect(mongoCtx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+
+	serverPort := 3333
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", serverPort))
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
 
-	fmt.Printf("Server listening at port: %d \n", port)
+	fmt.Printf("Server listening at port: %d \n", serverPort)
 	s := grpc.NewServer()
 	upvoteSystem.RegisterUpvoteSystemServer(s, &server{})
 
