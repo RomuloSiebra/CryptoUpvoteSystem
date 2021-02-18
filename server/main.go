5713284c7577e950b96f97f84d24ae3b0eb58284
RomuloSiebra <romulosiebra147@gmail.com>
2021-02-18 00:14:20 -0300

    Changed CreateCryptoRequest parameters, created mongo database and collection, implemented RPC CreateCrypto

--- server/main.go
+++ server/main.go
@@ -6,22 +6,48 @@ import (
 	"log"
 	"net"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
 	"go.mongodb.org/mongo-driver/mongo/options"
 
+	model "github.com/RomuloSiebra/CryptoUpvoteSystem/model"
 	upvoteSystem "github.com/RomuloSiebra/CryptoUpvoteSystem/proto/UpvoteSystem"
 	"go.mongodb.org/mongo-driver/mongo"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 var dbClient *mongo.Client
 var mongoCtx context.Context
+var db *mongo.Collection
 
 type server struct {
 	upvoteSystem.UnimplementedUpvoteSystemServer
 }
 
 func (*server) CreateCrypto(ctx context.Context, request *upvoteSystem.CreateCryptoRequest) (*upvoteSystem.CreateCryptoResponse, error) {
-	return nil, nil
+	crypto := request.GetCrypto()
+
+	data := model.Crypto{
+		ID:          primitive.NewObjectID(),
+		Name:        crypto.GetName(),
+		Description: crypto.GetDescription(),
+		Upvote:      0,
+		Downvote:    0,
+	}
+
+	insertResult, err := db.InsertOne(mongoCtx, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	crypto.Id = insertResult.InsertedID.(primitive.ObjectID).Hex()
+	crypto.Upvote = 0
+	crypto.Downvote = 0
+
+	response := &upvoteSystem.CreateCryptoResponse{Crypto: crypto}
+
+	return response, nil
 }
 
 func main() {
@@ -38,6 +64,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	db = dbClient.Database("UpvoteSystem").Collection("Cryptocurrency")
 	fmt.Println("Connected to MongoDB")
 
 	serverPort := 3333
@@ -48,6 +75,8 @@ func main() {
 
 	fmt.Printf("Server listening at port: %d \n", serverPort)
 	s := grpc.NewServer()
+
+	reflection.Register(s)
 	upvoteSystem.RegisterUpvoteSystemServer(s, &server{})
 
 	s.Serve(lis)
